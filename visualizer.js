fef78b1b08758faa9861f74f22ae05282884d684
Kavya Kumar <tellkavya@gmail.com>
2019-07-18 16:22:49 -0700

    Add outer circle

--- visualizer.js
+++ visualizer.js
@@ -2,9 +2,8 @@ window.onload = function() {
   
   var file = document.getElementById("thefile");
   var audio = document.getElementById("audio");
-  
+
   file.onchange = function() {
-  // audio.onload = function() {
     var files = this.files;
     audio.src = URL.createObjectURL(files[0]);
     audio.load();
@@ -16,7 +15,7 @@ window.onload = function() {
     var canvas = document.getElementById("canvas");
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-    var ctx = canvas.getContext("2d"), grd;
+    var ctx = canvas.getContext("2d");
 
     src.connect(analyser);
     analyser.connect(context.destination);
@@ -39,57 +38,69 @@ window.onload = function() {
     var barHeight;
     var x = 0;
 
+    // FTsignal = fft(signal - mean(signal)) / length(signal);
+    // [maxpeak, maxpeakindes] = max(abs(FTsignal) * 2);
+
+    // function returnOuterRadius() {
+    //   return Math.max(barHeight);
+    // }
+
     function renderFrame() {
       requestAnimationFrame(renderFrame);
       ctx.clearRect(0, 0, WIDTH, HEIGHT);
       x = Math.PI/2;
       barHeight = dataArray[0];
+
+      // maxBarHeight = Math.max(dataArray);
       analyser.getByteFrequencyData(dataArray);
-      
-      grd = ctx.createRadialGradient(150, 150, 0, 150, 150, 150);
-      grd.addColorStop(0, "red");
-      grd.addColorStop(0.2, "white");
-      grd.addColorStop(0.7, "green");
+
+      grd = ctx.createRadialGradient(centerX, centerY, 40, centerX, centerY, 100);
+      grd.addColorStop(0, 'rgba(41, 10, 89, 1.000)');
+      grd.addColorStop(0.3, 'rgba(255, 124, 0, 1.000)');
+      grd.addColorStop(0.6, "#8E4142");
       // ctx.fillStyle = "blue";
-      // ctx.strokeStyle = 'rgb(102, 204, 0)';
-      ctx.strokeStyle = 'rgb(0, ' + Math.floor(255 - 42.5 * x) + ', ' +
-        Math.floor(255 - 42.5 * x * 2) + ')';
-      
+      // ctx.strokeStyle = 'rgb(0, ' + Math.floor(255 - 42.5 * x) + ', ' +
+      //   Math.floor(255 - 42.5 * x * 2) + ')';
       // ctx.fillRect(0, 0, WIDTH, HEIGHT);
-      
       for (var i = 0; i < bufferLength; i++) {
         barHeight = dataArray[i];
-
-
-        
-        
         ctx.beginPath();
         ctx.moveTo(centerX, centerY);
         ctx.lineTo(centerX + (barHeight * Math.cos(x)), centerY + (barHeight * Math.sin(x)));
         ctx.lineTo(centerX + (barHeight * Math.cos(x + barWidth)), centerY + (barHeight * Math.sin(x + barWidth)));
         ctx.lineTo(centerX, centerY);
         ctx.closePath();
-        
+        ctx.strokeStyle = grd;
+        ctx.stroke();
+      
         //Fill Style
-        ctx.fillStyle = grd;
+        // grd = ctx.createLinearGradient(50, 50, 0, 150, 150);
+        // var grd = ctx.createRadialGradient(150, 150, 100, 100, 100, 50);
+        // grd.addColorStop(0, "red");
+        // grd.addColorStop(1, "green");
         // ctx.fillStyle= "blue";
-        ctx.fill();
-
-
+        
+        
         // Stroke Style
         // ctx.strokeStyle = 'rgb(0, ' + Math.floor(255 - 42.5 * i * 5) + ', ' +
         //   Math.floor(255 - 42.5 * i * 10) + ')';
-        // ctx.stroke()
-
+        
         // Old Code
         // var r = barHeight + (25 * (i/bufferLength));
         // var g = 250 * (i/bufferLength);
         // var b = 50;
         // ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
         // ctx.fillRect(x, HEIGHT - barHeight, barWidth, barHeight);
-
         x += barWidth;
       }
+      var maxBarHeight = dataArray[0];
+      if (barHeight > maxBarHeight) {
+        maxBarHeight = barHeight;
+      }
+      ctx.beginPath();
+      ctx.arc(centerX, centerY, maxBarHeight*1.35, 0, 2 * Math.PI);
+      ctx.strokeStyle = 'rgba(41, 10, 89, 1.000)';
+      ctx.stroke();
     }
 
     audio.play();
